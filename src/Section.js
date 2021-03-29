b6d4cb1d9e3f3b010cff699ed6e89c13e83684d1
Marques <MBatoon1996@gmail.com>
2021-03-29 04:35:55 -1000

    added view kanji only mode as well as reading only mode

--- src/Section.js
+++ src/Section.js
@@ -13,6 +13,8 @@ class Section extends React.Component {
             page: characters[params.get('章')][0],
             pageChars: '',
             displayClass: 'd-none',
+            readClass: 'py-2 d-block',
+            mode: '読み',
         };
     }
 
@@ -24,32 +26,50 @@ class Section extends React.Component {
         this.setState({ displayClass: 'd-none'});
     }
 
+    changeMode = (event) => {
+        this.setState({ mode: event.target.value });
+        if(event.target.value === "漢字"){
+            this.setState({ readClass: "d-none", displayClass: 'd-block' });
+        }
+        else if(event.target.value === "読み"){
+            this.setState({ readClass: "py-2 d-block", displayClass: 'd-none' });
+        }
+    }
+
     changePage = (event) => {
         console.log("page is " + event.target.value);
         console.log(characters[this.state.章][event.target.value]);
         this.setState({ 
             page: event.target.value,
-            pageChars: characters[this.state.章][event.target.value] });
+            pageChars: characters[this.state.章][event.target.value], });
+        if(this.state.mode==="読み")
+            this.setState({ displayClass: 'd-none' });
     }
 
     render() {
-        const{ 章, page, pages, pageChars, displayClass } = this.state;
+        const{ 章, page, pages, pageChars, displayClass, readClass, mode} = this.state;
         const chars = Object.keys(pages).map(pgChoice => <option key={pgChoice} value={pgChoice}>{pgChoice}</option>)
         return(
             <React.Fragment>
                 <h1 className="text-center py-3">{章}</h1>
                 <form className="form-row p-3 justify-content-center">
-                    <div className="form-group col-md-5 mb-0">
+                    <div className="form-group col-8 mb-0">
                         <select value={page} className="form-control form-control-lg mb-2" onChange={this.changePage}>
                             {chars}
+                        </select>
+                    </div>
+                    <div className="form-group col-4 mb-0">
+                        <select value={mode} className="form-control form-control-lg mb-2" onChange={this.changeMode}>
+                            <option value="読み">読み</option>
+                            <option value="漢字">漢字</option>
                         </select>
                     </div>
                 </form>
                 {Object.keys(pageChars).map(kanji => 
                     <div className="py-3">
-                        <h4 className={displayClass}>{kanji}</h4>
-                        <h5 className="py-3">訓読み：{pageChars[kanji]['訓読み']}</h5>
-                        <h5 className="py-3">音読み：{pageChars[kanji]['音読み']}</h5>
+                        <h3 className={displayClass}>{kanji}</h3>
+                        <h5 className={readClass}>訓読み：{pageChars[kanji]['訓読み']}</h5>
+                        <h5 className={readClass}>音読み：{pageChars[kanji]['音読み']}</h5>
                     </div>
                 )}
                 <div className="container">
