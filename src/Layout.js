027f264c88450693a14bd8f06a2f5783f155a6cf
Marques <MBatoon1996@gmail.com>
2021-03-28 15:57:11 -1000

    added FontAwesome and made footer

--- src/Layout.js
+++ src/Layout.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(fab);
 
 const Layout = (props) => {
     return (
         <React.Fragment>
             <nav className="navbar navbar-light  bg-light">
-                <span className="navbar-brand mb-0 h1">N3漢字</span>
+                <Link to="/"><span className="navbar-brand mb-0 h1">N3漢字</span></Link>
             </nav>
             <div className="container py-3">
                 {props.children}
             </div>
+            <footer className="page-footer py-3 bg-light">
+                <div className="container-fluid">
+                    <div className="row justify-content-center">
+                        <div className="col-12 justify-content-center row">
+                            <a href="https://www.instagram.com/batoonworld/" rel="noreferrer" target="_blank" className="mx-1">
+                            <FontAwesomeIcon icon={['fab', 'instagram']} />
+                            </a>
+                            <a href="https://twitter.com/batoonworld" rel="noreferrer" target="_blank" className="mx-1">
+                            <FontAwesomeIcon icon={['fab', 'twitter']} />
+                            </a>
+                            <a href="https://www.github.com/MBatoon1996" rel="noreferrer" target="_blank" className="mx-1">
+                            <FontAwesomeIcon icon={['fab', 'github']} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <p className="text-center">©2021 Marques Batoon</p>
+            </footer>
         </React.Fragment>
     );
 }
